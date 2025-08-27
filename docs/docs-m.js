f9b332780a00b23510d89a3ace7e800968099c9a
Y_z00 <yyzz00@foxmail.com>
2025-08-28 00:59:02 +0800

    i cant image that what im want to do

--- docs/docs-m.js
+++ docs/docs-m.js
@@ -1,9 +1,41 @@
 let onSwitch = true;
-document.querySelector('.body-sidebar-icons').addEventListener("click", (event) => {
-    if(onSwitch) {
-        document.getElementById('body-sidebar-icons-1').style.transform="rotate(90deg)";
-    }else{
-        document.getElementById('body-sidebar-icons-1').style.transform="rotate(0deg)";
+document
+  .querySelector("#body-sidebar-icons-1")
+  .addEventListener("click", (event) => {
+    if (onSwitch) {
+      document.getElementById("body-sidebar-icons-1").style.transform =
+        "rotate(90deg)";
+      document.getElementById("body-sidebar-submenu-1").style.display = "block";
+    } else {
+      document.getElementById("body-sidebar-icons-1").style.transform =
+        "rotate(0deg)";
+      document.getElementById("body-sidebar-submenu-1").style.display = "none";
     }
-    onSwitch = !onSwitch
-});
+    onSwitch = !onSwitch;
+  });
+
+  //function to change the list style
+  // function lisStyle(){
+
+  // }
+
+let onLis = true;
+var lis=document.getElementsByClassName("body-sidebar-lis");
+document
+  .querySelector(".body-sidebar-title")
+  .addEventListener("click", (event) => {
+    if (onLis) {
+      for(var i = 0; i < lis.length;i++) {
+        lis[i].style.display="none";
+      }
+      document.getElementById("body-sidebar-icons-3").style.transform =
+      "rotate(0deg)";
+    } else {
+      for(var i = 0; i < lis.length;i++) {
+        lis[i].style.display="block";
+      }
+      document.getElementById("body-sidebar-icons-3").style.transform =
+      "rotate(90deg)";
+    }
+    onLis = !onLis;
+  });
