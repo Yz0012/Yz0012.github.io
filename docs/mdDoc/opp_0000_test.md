fa48f06e1a895670341a8d686b39d555a7586690
Yz0012 <2277509815@qq.com>
2025-10-26 22:56:56 +0800

    docs:add more message

---
+++ docs/mdDoc/opp_0000_test.md
@@ -0,0 +1,21 @@
+# Object-Oriented Programming (OOP)
+
+Object-Oriented Programming is a programming paradigm based on the concept of "objects" that contain data and code. The data is in the form of fields (attributes), and the code is in the form of procedures (methods).
+
+## Key Concepts
+
+1. **Classes and Objects**
+    - Class: A blueprint for creating objects
+    - Object: An instance of a class
+
+2. **Four Pillars of OOP**
+    - Encapsulation: Bundling data and methods that operate on that data
+    - Inheritance: Creating new classes based on existing ones
+    - Polymorphism: Ability to present the same interface for different types
+    - Abstraction: Hiding complex implementation details
+
+## Benefits
+- Reusability through inheritance
+- Modularity and maintainable code
+- Better organization of code
+- Secure data through encapsulation
--- genFiles/genDoc.js
+++ genFiles/genDoc.js
@@ -1,5 +1,5 @@
 import MarkdownIt from "markdown-it";
-import { readdir, stat, readFileSync, writeFileSync } from "fs";
+import { readdir, stat, readFileSync, writeFileSync, mkdirSync, existsSync } from "fs";
 import { resolve, join } from "path";
 import jsdom from "jsdom";
 const { JSDOM } = jsdom;
@@ -9,12 +9,6 @@ const md = new MarkdownIt({
   html: true,
   linkify: true,
   typographer: true
-});
-
-//you know what im doing _2
-const dom = new JSDOM(mainHtml, {
-  runScripts: "dangerously",
-  resources: "usable",
 });
 
 fileDisplay(pathSel);
@@ -33,18 +27,33 @@ function fileDisplay(filePath) {
           const isFile = stats.isFile();
           const isDir = stats.isDirectory();
           if (isFile) {
-            const md_Doc_Src = readFileSync(
+            let md_Doc_Src = readFileSync(
               "./docs/mdDoc/" + fileName,
               "utf-8"
             );
-            const html = md.render(md_Doc_Src);
+            //you know what im doing _2
+            const dom = new JSDOM(mainHtml, {
+              runScripts: "dangerously",
+              resources: "usable",
+            });
+            console.log(md_Doc_Src);
+            let html = md.render(md_Doc_Src);
             let a = dom.window.document.createElement("div");
             a.innerHTML = html;
             dom.window.document.getElementById("body_0").appendChild(a);
+            let newDir = "./docs/htmlDoc/" + "html_" + fileName.split("_")[0];
+            if (existsSync(newDir)) {
+              console.log("The directory exists. Path:" + newDir);
+            } else {
+              mkdirSync(newDir, (err) => {
+                if (err) throw err;
+                console.log("success!");
+              });
+            }
             setTimeout(() => {
               writeFileSync(
                 //命名规定:第一个字符 "_" 前为分类的文件类型，后为该类型文件的数字标号，同时表示文件数，第二个字符 "_" 后为文件名
-                "./docs/htmlDoc/html_" + fileName.split("_")[0] + "/" + fileName.split(".")[0] + ".html",//创建新文件夹
+                newDir + "/" + fileName.split(".")[0] + ".html",//写到新文件夹
                 dom.serialize()
               );
               console.log(
